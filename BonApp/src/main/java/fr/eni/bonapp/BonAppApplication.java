6cc9c4660f48aec887d4b6f4be58d6b6d7f2c397
ben-ken0bi <ben8waters@gmail.com>
2023-12-18 14:06:24 +0100

    Initial commit

---
+++ BonApp/src/main/java/fr/eni/bonapp/BonAppApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.bonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonAppApplication.class, args);
+	}
+
+}
---
+++ BonApp/src/test/java/fr/eni/bonapp/BonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.bonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
