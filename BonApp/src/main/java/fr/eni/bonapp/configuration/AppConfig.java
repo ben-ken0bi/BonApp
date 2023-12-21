1bed4660b4dbe8ccb157ea7ea9a9040fdc1100d7
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2023-12-21 14:12:49 +0100

    ajout connection à la base de donnée

---
+++ BonApp/src/main/java/fr/eni/bonapp/configuration/AppConfig.java
@@ -0,0 +1,21 @@
+package fr.eni.bonapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+class AppConfig {
+
+    private AppConfig(){
+        throw new IllegalStateException("Classe de configuration");
+    }
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource(".env"));
+        return configurer;
+    }
+
+}
