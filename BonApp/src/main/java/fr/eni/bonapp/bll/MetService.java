fc534b643a8733524ae1d7afb359aa2ba53371cb
ben-ken0bi <ben8waters@gmail.com>
2023-12-22 09:25:28 +0100

    feature/lister-mets BO/DAO/Service

--- BonApp/src/main/java/fr/eni/bonapp/bll/MetService.java
+++ BonApp/src/main/java/fr/eni/bonapp/bll/MetService.java
@@ -1,11 +1,12 @@
-package fr.eni.bonapp.dal;
+package fr.eni.bonapp.bll;
 
 import fr.eni.bonapp.bo.Met;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MetDAO {
+public interface MetService {
     Optional<Met> chercherMetParId(long idMet);
     List<Met> listerMets();
+
 }
