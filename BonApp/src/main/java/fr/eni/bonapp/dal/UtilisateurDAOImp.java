eaa8a8dc4e5a2b11a600861c40bde8725a2d3198
lcaffarelli2022 <laetitia.caffarelli@gmail.com>
2024-01-03 17:09:42 +0100

    correction de la pr connexion

--- BonApp/src/main/java/fr/eni/bonapp/dal/UtilisateurDAOImp.java
+++ BonApp/src/main/java/fr/eni/bonapp/dal/UtilisateurDAOImp.java
@@ -1,9 +1,6 @@
 package fr.eni.bonapp.dal;
 
 import fr.eni.bonapp.bo.Utilisateur;
-import java.sql.ResultSet;
-import java.util.List;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,94 +8,92 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class UtilisateurDAOImp implements UtilisateurDAO {
-  Logger logger = LoggerFactory.getLogger(UtilisateurDAOImp.class);
-  private JdbcTemplate jdbcTemplate;
+    Logger logger = LoggerFactory.getLogger(UtilisateurDAOImp.class);
+    private JdbcTemplate jdbcTemplate;
 
-  @Autowired
-  public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
-  }
-
-  /**
-   * Permet de chercher un utilisateur via son id. Si aucun utilisateur n'est trouvé, renvoie un
-   * logger le precisant et un optional vide.
-   * @param idUtilisateur
-   * @return
-   */
-  @Override
-  public Optional<Utilisateur> chercherUtilisateurParId(long idUtilisateur) {
-    logger.info("Dans Utilisateur avec la methode pour trouver son id numéro {}", idUtilisateur);
-    String sql =
-        "Select id_utilisateur, pseudo, mdp, nom, prenom, email, admin from utilisateur where id_utilisateur =?";
-    Optional<Utilisateur> optUtilisateur = Optional.empty();
-
-    try {
-      Utilisateur utilisateur =
-          jdbcTemplate.queryForObject(
-              sql,
-              (ResultSet rs, int rowNum) ->
-                  new Utilisateur(
-                      rs.getLong(1),
-                      rs.getString(2),
-                      rs.getString(3),
-                      rs.getString(4),
-                      rs.getString(5),
-                      rs.getString(6),
-                      rs.getBoolean(7)),
-              idUtilisateur);
-      optUtilisateur = Optional.of(utilisateur);
-    } catch (DataAccessException dae) {
-      logger.error("Aucun utilisateur existe avec l'id suivant : {}", idUtilisateur);
-      return Optional.empty();
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-    return optUtilisateur;
-  }
 
-  @Override
-  public Optional<Utilisateur> chercherUtilisateurParPseudo(String pseudo) {
-    logger.info("Dans Utilisateur avec la methode pour trouver son pseudo {}", pseudo);
-    String sql =
-        "Select id_utilisateur,nom,prenom, pseudo,mdp, email, admin from utilisateur where pseudo =?";
-    Optional<Utilisateur> optUtilisateur = Optional.empty();
+    /**
+     * Permet de chercher un utilisateur via son id. Si aucun utilisateur n'est trouvé, renvoie un
+     * logger le precisant et un optional vide.
+     *
+     * @param idUtilisateur
+     * @return
+     */
+    @Override
+    public Optional<Utilisateur> chercherUtilisateurParId(long idUtilisateur) {
+        logger.info("Dans Utilisateur avec la methode pour trouver son id numéro {}", idUtilisateur);
+        String sql =
+                "Select id_utilisateur, pseudo, mdp, nom, prenom, email, admin from utilisateur where id_utilisateur =?";
+        Optional<Utilisateur> optUtilisateur = Optional.empty();
 
-    try {
-      Utilisateur utilisateur =
-          jdbcTemplate.queryForObject(
-              sql,
-              (ResultSet rs, int rowNum) ->
-                  new Utilisateur(
-                      rs.getLong(1),
-                      rs.getString(2),
-                      rs.getString(3),
-                      rs.getString(4),
-                      rs.getString(5),
-                      rs.getString(6),
-                      rs.getBoolean(7)),
-              pseudo);
-
-//      this.idUtilisateur = idUtilisateur;
-//      this.nom = nom;
-//      this.prenom = prenom;
-//      this.pseudo = pseudo;
-//      this.mdp = mdp;
-//      this.email = email;
-//      this.admin = admin;
-      optUtilisateur = Optional.of(utilisateur);
-    } catch (DataAccessException dae) {
-      logger.error("Aucun utilisateur existe avec le pseudo suivant : {}", pseudo);
+        try {
+            Utilisateur utilisateur =
+                    jdbcTemplate.queryForObject(
+                            sql,
+                            (ResultSet rs, int rowNum) ->
+                                    new Utilisateur(
+                                            rs.getLong(1),
+                                            rs.getString(2),
+                                            rs.getString(3),
+                                            rs.getString(4),
+                                            rs.getString(5),
+                                            rs.getString(6),
+                                            rs.getBoolean(7)),
+                            idUtilisateur);
+            optUtilisateur = Optional.of(utilisateur);
+        } catch (DataAccessException dae) {
+            logger.error("Aucun utilisateur existe avec l'id suivant : {}", idUtilisateur);
+            return Optional.empty();
+        }
+        return optUtilisateur;
     }
-    return optUtilisateur;
-  }
 
-  @Override
-  public List<Utilisateur> listerUtilisateurs() {
-    return null;
-  }
+    @Override
+    public Optional<Utilisateur> chercherUtilisateurParPseudo(String pseudo) {
+        logger.info("Dans Utilisateur avec la methode pour trouver son pseudo {}", pseudo);
+        String sql =
+                "Select id_utilisateur,nom,prenom, pseudo,mdp, email, admin from utilisateur where pseudo =?";
+        Optional<Utilisateur> optUtilisateur = Optional.empty();
 
-  @Override
-  public Optional<Utilisateur> chercherUtilisateurParRecette(long idRecette) {
-    return Optional.empty();
-  }
+        try {
+            Utilisateur utilisateur =
+                    jdbcTemplate.queryForObject(
+                            sql,
+                            (ResultSet rs, int rowNum) ->
+                                    new Utilisateur(
+                                            rs.getLong(1),
+                                            rs.getString(2),
+                                            rs.getString(3),
+                                            rs.getString(4),
+                                            rs.getString(5),
+                                            rs.getString(6),
+                                            rs.getBoolean(7)),
+                            pseudo);
+
+            optUtilisateur = Optional.of(utilisateur);
+        } catch (DataAccessException dae) {
+            logger.error("Aucun utilisateur existe avec le pseudo suivant : {}", pseudo);
+        }
+        return optUtilisateur;
+    }
+
+    @Override
+    public List<Utilisateur> listerUtilisateurs() {
+        return null;
+    }
+
+    @Override
+    public Optional<Utilisateur> chercherUtilisateurParRecette(long idRecette) {
+        return Optional.empty();
+    }
 }
